e2199a6f970a6c8291718c2feadc59e7c75b21a7
Riwji5588 <riwaurov@gmail.com>
2021-11-09 16:39:03 +0700

    first commit

---
+++ plugin/config.php
@@ -0,0 +1,8 @@
+<?php
+
+$db_cofig = array();
+$db_cofig['db_host'] = '127.0.0.1'; //SQL HOST
+$db_cofig['db_user'] = 'root'; //SQL USERNAME
+$db_cofig['db_pass'] = ''; //SQL PASSWORD
+$db_cofig['db_name'] = 'database_hart'; //SQL DATABASE TABLE
+?>
