aa09a8ca490b967c65eae9d7d63469cdfb60d3a2
DisEzMike <mikenatchapon@gmail.com>
2021-11-09 22:12:32 +0700

    edit game data and add strsub for stock id

--- plugin/add_new_data.php
+++ plugin/add_new_data.php
@@ -3,51 +3,47 @@
 include("hyper_api.php");
 $errorMSG = "";
 
-if(isset($_POST['gameidnew'])){
-   
-    if(empty($_POST['usernamenew'])){
+if (isset($_POST['gameidnew'])) {
+
+    if (empty($_POST['stockId'])) {
         $errorMSG = "กรุณากรอก ชื่อผู้ใช้งาน";
-    }else{
+    } else {
+        if (empty($_POST['cardnew']) || $_POST['cardnew'] == 0) {
+            $errorMSG = "กรุณาเลือก ที่อยู่ข้อมูล";
+        } else {
 
-        if(empty($_POST['passwordnew'])){
-            $errorMSG = "กรุณากรอก รหัสผ่าน";
-        }else{
-    
-            if(empty($_POST['cardnew']) || $_POST['cardnew'] == 0){
-                $errorMSG = "กรุณาเลือก ที่อยู่ข้อมูล";
-            }else{
-        
-                if(empty($_POST['detailnew'])){
-                    $_POST['detailnew'] = "";
-                }
-
-                $gid = $_POST['gameidnew'];
-                $user = $_POST['usernamenew'];
-                $pass = base64_encode($_POST['passwordnew']);
-                $cid = $_POST['cardnew'];
-                $detail = $_POST['detailnew'];
-
-                $add_new_data = "INSERT INTO game_data (game_id, card_id, username, password, detail) VALUES ('$gid','$cid','$user','$pass','$detail')";
-                $result = $hyper->connect->query($add_new_data);
-                if(!$result){
-                    $errorMSG = "เพิ่มข้อมูลไม่สำเร็จ";
-                }
-        
+            if (empty($_POST['detailnew'])) {
+                $_POST['detailnew'] = "";
             }
-    
+
+            $gid = $_POST['gameidnew'];
+            $allId = $_POST['stockId'];
+            $cid = $_POST['cardnew'];
+            $detail = $_POST['detailnew'];
+            
+            $arr = explode("\n", $allId);
+            $arr_ = array();
+            $obj = array();
+            foreach ($arr as $val) {
+                $user = substr($val, 0, strpos($val, ':'));
+                $passbetweendisplay = substr($val, strpos($val, ':') + 1);
+                $pass = base64_encode(substr($passbetweendisplay, 0, strpos($passbetweendisplay, ':')));
+                $display = substr($passbetweendisplay, strpos($passbetweendisplay, ':') + 1);
+                $add_new_data = "INSERT INTO game_data (game_id, card_id, username, password, display, detail) VALUES ('$gid','$cid','$user','$pass','$display','$detail')";
+                $result = $hyper->connect->query($add_new_data);
+            }
+            if (!$result) {
+                $errorMSG = "เพิ่มข้อมูลไม่สำเร็จ";
+            }
         }
-
     }
-    
+
     /* result */
-    if(empty($errorMSG)){
-        echo json_encode(['code'=>200,]);
-    }else{
-        echo json_encode(['code'=>500, 'msg'=>$errorMSG]);
+    if (empty($errorMSG)) {
+        echo json_encode(['code' => 200,]);
+    } else {
+        echo json_encode(['code' => 500, 'msg' => $errorMSG]);
     }
-
-}else{
-  header("Location: 403.php");
+} else {
+    header("Location: 403.php");
 }
-
-?>
