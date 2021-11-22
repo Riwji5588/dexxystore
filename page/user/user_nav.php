f7b4784325f724430fc06210c731c3702a9ae863
DisEzMike <mikenatchapon@gmail.com>
2021-11-23 00:18:15 +0700

    edit navbar

--- page/user/user_nav.php
+++ page/user/user_nav.php
@@ -1,36 +1,72 @@
-<nav class="navbar navbar-expand-md navbar-dark  fixed-top" style="background-color: #131315;">
-  <a class="navbar-brand" href="home">
-    <img src="assets/img/logo_dexyStore.jpg" width="64" height="64" class="d-inline-block align-top rounded-circle">
-  </a>
+<!-- <nav class="navbar navbar-expand-md navbar-dark fixed-top" style="background-color: #131315;">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
+  <a class="navbar-brand" href="home">
+    <img src="assets/img/logo_dexyStore.jpg" width="64" height="64" class="d-inline-block align-top rounded-circle">
+  </a>
 
-  <div class="collapse navbar-collapse" id="navbarTogglerDemo02" style="margin-right: 30%;">
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0"></ul>
-    <div class="form-inline my-2 my-lg-0">
-
-      <a href="home"><button class="btn btn-small pu <?php if ($page == 'home' || $page == 'shop' || $page == 'item') {
-                                                        echo '-active';
-                                                      } ?> my-2 my-sm-0 mr-2" type="button"><i class="fal fa-home-lg-alt mr-1"></i> หน้าแรก</button></a>
-      <a href="profile"><button class="btn btn-sm gr <?php if ($page == 'profile') {
-                                                        echo '-active';
-                                                      } ?> my-2 my-sm-0 mr-2" type="butt><i class=" fal fa-user mr-1"></i> บัญชีของฉัน</button></a>
-      <?php if ($data_user['role'] == '779') { ?><a href="adminsys"><button class="btn btn-sm pk <?php if ($page == 'adminsys' || $page == 'gametype' || $page == 'gameselect' || $page == 'editgame' || $page == 'gamecard' || $page == 'gamedata' || $page == 'dataowner' || $page == 'datauser' || $page == 'datapay' || $page == 'websetting' || $page == 'report') {
-                                                                                                    echo '-active';
-                                                                                                  } ?> my-2 my-sm-0 mr-2" type="button"><i class="fal fa-tools mr-1"></i> ระบบแอดมิน</button></a><?php } ?>
-      <a href="history"><button class="btn btn-sm yl <?php if ($page == 'history') {
-                                                        echo '-active';
-                                                      } ?> my-2 my-sm-0 mr-2" type="button"><i class="fal fa-history mr-1"></i> ประวัติการซื้อ</button></a>
-      <a href="topup"><button class="btn btn-sm or <?php if ($page == 'topup') {
-                                                      echo '-active';
-                                                    } ?> my-2 my-sm-0 mr-2" type="button"><i class="fal fa-credit-card mr-1" style="color: white;"></i> เติมเงิน</button></a>
-      <a href="https://twitter.com/dexy_store/" target="_blank"><button class="btn btn-sm bl my-2 my-sm-0 mr-2" type="button"><i class="fab fa-twitter-square mr-1"></i> Twitter</button></a>
-      <a href="logout"><button class="btn btn-sm rd  my-2 my-sm-0 mr-3" type="button"><i class="fad fa-times-circle mr-1"></i> ออกจากระบบ</button></a>
-
+  <div class="mx-auto order-0">
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02" align="center">
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li><a href="home"><button class="btn btn-sm pu <?php if ($page == 'home' || $page == 'shop' || $page == 'item') {
+                                                          echo '-active';
+                                                        } ?> my-2 my-sm-0 mr-2" type="button"><i class="fal fa-home-lg-alt mr-1"></i> หน้าแรก</button></a></li>
+        <li><a href="profile"><button class="btn btn-sm gr <?php if ($page == 'profile') {
+                                                              echo '-active';
+                                                            } ?> my-2 my-sm-0 mr-2" type="butt><i class=" fal fa-user mr-1"></i> บัญชีของฉัน</button></a></li>
+        <?php if ($data_user['role'] == '779') { ?><li><a href="adminsys"><button class="btn btn-sm pk <?php if ($page == 'adminsys' || $page == 'gametype' || $page == 'gameselect' || $page == 'editgame' || $page == 'gamecard' || $page == 'gamedata' || $page == 'dataowner' || $page == 'datauser' || $page == 'datapay' || $page == 'websetting' || $page == 'report') {
+                                                                                                          echo '-active';
+                                                                                                        } ?> my-2 my-sm-0 mr-2" type="button"><i class="fal fa-tools mr-1"></i> ระบบแอดมิน</button></a></li><?php } ?>
+        <li><a href="history"><button class="btn btn-sm yl <?php if ($page == 'history') {
+                                                              echo '-active';
+                                                            } ?> my-2 my-sm-0 mr-2" type="button"><i class="fal fa-history mr-1"></i> ประวัติการซื้อ</button></a></li>
+        <li><a href="topup"><button class="btn btn-sm or <?php if ($page == 'topup') {
+                                                            echo '-active';
+                                                          } ?> my-2 my-sm-0 mr-2" type="button"><i class="fal fa-credit-card mr-1" style="color: white;"></i> เติมเงิน</button></a></li>
+        <li><a href="https://twitter.com/dexy_store/" target="_blank"><button class="btn btn-sm bl my-2 my-sm-0 mr-2" type="button"><i class="fab fa-twitter-square mr-1"></i> Twitter</button></a></li>
+        <li><a href="logout"><button class="btn btn-sm rd  my-2 my-sm-0 mr-3" type="button"><i class="fad fa-times-circle mr-1"></i> ออกจากระบบ</button></a></li>
+      </ul>
     </div>
   </div>
+
+</nav> -->
+
+<nav class="navbar navbar-expand-md navbar-dark fixed-top justify-content-center" style="background-color: #131315;">
+  <a class="navbar-brand" href="home">
+    <img src="assets/img/logo_dexyStore.jpg" width="64" height="64" class="d-inline-block align-top rounded-circle">
+  </a>
+  <button class="navbar-toggler ml-1" type="button" data-toggle="collapse" data-target="#collapsingNavbar2">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="navbar-collapse collapse justify-content-between align-items-center w-100" id="collapsingNavbar2">
+    <ul class="navbar-nav mx-auto text-center">
+      <li><a href="home"><button class="btn btn-sm pu <?php if ($page == 'home' || $page == 'shop' || $page == 'item') {
+                                                        echo '-active';
+                                                      } ?> my-2 my-sm-0 mr-2" type="button"><i class="fal fa-home-lg-alt mr-1"></i> หน้าแรก</button></a></li>
+      <li><a href="profile"><button class="btn btn-sm gr <?php if ($page == 'profile') {
+                                                            echo '-active';
+                                                          } ?> my-2 my-sm-0 mr-2" type="butt><i class=" fal fa-user mr-1"></i> บัญชีของฉัน</button></a></li>
+      <?php if ($data_user['role'] == '779') { ?><li><a href="adminsys"><button class="btn btn-sm pk <?php if ($page == 'adminsys' || $page == 'gametype' || $page == 'gameselect' || $page == 'editgame' || $page == 'gamecard' || $page == 'gamedata' || $page == 'dataowner' || $page == 'datauser' || $page == 'datapay' || $page == 'websetting' || $page == 'report') {
+                                                                                                        echo '-active';
+                                                                                                      } ?> my-2 my-sm-0 mr-2" type="button"><i class="fal fa-tools mr-1"></i> ระบบแอดมิน</button></a></li><?php } ?>
+      <li><a href="history"><button class="btn btn-sm yl <?php if ($page == 'history') {
+                                                            echo '-active';
+                                                          } ?> my-2 my-sm-0 mr-2" type="button"><i class="fal fa-history mr-1"></i> ประวัติการซื้อ</button></a></li>
+      <li><a href="topup"><button class="btn btn-sm or <?php if ($page == 'topup') {
+                                                          echo '-active';
+                                                        } ?> my-2 my-sm-0 mr-2" type="button"><i class="fal fa-credit-card mr-1" style="color: white;"></i> เติมเงิน</button></a></li>
+      <li><a href="https://twitter.com/dexy_store/" target="_blank"><button class="btn btn-sm bl my-2 my-sm-0 mr-2" type="button"><i class="fab fa-twitter-square mr-1"></i> Twitter</button></a></li>
+      <li><a href="logout"><button class="btn btn-sm rd  my-2 my-sm-0 mr-3" type="button"><i class="fad fa-times-circle mr-1"></i> ออกจากระบบ</button></a></li>
+    </ul>
+    <!-- <ul class="nav navbar-nav flex-row justify-content-center flex-nowrap">
+      <li class="nav-item"><a class="nav-link" href=""><i class="fa fa-facebook mr-1"></i></a> </li>
+      <li class="nav-item"><a class="nav-link" href=""><i class="fa fa-twitter"></i></a> </li>
+    </ul> -->
+  </div>
 </nav>
+
+
 
 <style>
   .btn {
--- page/welcome.php
+++ page/welcome.php
@@ -1,6 +1,6 @@
 <!-- Game Type -->
-<h1 class="text-center mt-4 mb-2" style="color: white;">Netflixพร้อมจำหน่าย</h1>
-<div class="row no-gutters">
+<h1 class="text-center mt-4 mb-2" style="color: white;">Netflixพร้อมจำหน่าย</h1><br>
+<div class="row no-gutters" style="justify-content: center;">
 
   <?php
   $sql_select_game = "SELECT * FROM game_type ORDER BY game_id DESC";
@@ -21,32 +21,51 @@
       if ($ready_selled_row['totaldata'] > 0) {
 
     ?>
-        <div class="col-6 col-lg-4 p-2">
+        <div class="col-12 col-md-6 col-lg-4 p-2">
           <a href="shop&gameid=<?= $game['game_id']; ?>">
-            <div class="card shadow-dark radius-border-6 hyper-bg-white text-center p-2 h-100 hyper-card">
-              <div class="media">
-                <img src="assets/img/game/<?= $game['game_image']; ?>" class="align-self-center mr-3 rounded-circle d-none d-md-block" width="70px;">
-                <div class="media-body text-center text-md-left">
-                  <img src="assets/img/game/<?= $game['game_image']; ?>" class="ml-auto mr-auto rounded-circle d-block d-md-none" width="70px;">
-                  <h4 class="mt-0 mb-1"><?= $game['game_name']; ?></h4>
-                  <font class="text-muted">พร้อมส่ง <?= number_format($ready_selled_row['totaldata'], 0); ?> จอ</font>
-                </div>
+            <div class="card shadow-dark radius-border-6 hyper-bg-white hyper-card">
+              <img src="assets/img/game/<?= $game['game_image']; ?>" class="card-img-top img-fluid" style="border-top-left-radius: 0.6rem !important;border-top-right-radius: 0.6rem !important;">
+              <div class="card-body">
+                <h5 class="mt-0 mb-2" id="title<?= $game['game_id']; ?>"><?= $game['game_name']; ?></h5>
+                <span style="color: green;">พร้อมส่ง <?= number_format($ready_selled_row['totaldata'], 0); ?> จอ</span>
               </div>
             </div>
           </a>
         </div>
+
   <?php }
     } while ($game = mysqli_fetch_array($query_game));
   } ?>
 
-<!-- End Game Type -->
 
-<style>
-  body {
-    background-color: #131315;
-  }
 
-  .color {
-    background-color: #131315;
-  }
-</style>
+
+  <!-- End Game Type -->
+
+  <style>
+    body {
+      background-color: #131315;
+    }
+
+    .color {
+      background-color: #131315;
+    }
+
+    /* .hover:hover {
+      box-shadow: 0px 0px 10px #fff;
+    } */
+
+    #hover:hover {
+      color: #b67bfb;
+      box-shadow:
+        0 0 1px #b67bfb,
+        0 0 4px #b67bfb,
+        0 0 5px #b67bfb,
+        0 0 6px #b67bfb,
+        0 0 8px #b67bfb,
+        0 0 10px #b67bfb,
+        0 0 20px #b67bfb,
+        0 0 25px #b67bfb;
+      animation: pulsate 1.2s infinite alternate;
+    }
+  </style>
