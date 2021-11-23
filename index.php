a8699b55571e5f960bea45e8c63a05f0d053a64f
DisEzMike <mikenatchapon@gmail.com>
2021-11-23 16:07:00 +0700

    link

--- index.php
+++ index.php
@@ -80,6 +80,7 @@ function DateThai($strDate)
   <link href="assets/css/bootstrap.css" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="assets/css/jquery.dataTables.min.css">
   <link rel="stylesheet" type="text/css" href="assets/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
   <link href="assets/css/animate.css" rel="stylesheet">
   <script src="assets/js/sweetalert.min.js"></script>
   <link href="assets/css/hyper.css" rel="stylesheet">
