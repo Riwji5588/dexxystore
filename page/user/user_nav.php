0ef55589ed4fc6f6bad9c1254d0144b4e6283119
DisEzMike <mikenatchapon@gmail.com>
2021-11-26 08:40:46 +0700

    edit nav

--- page/user/user_nav.php
+++ page/user/user_nav.php
@@ -22,14 +22,20 @@ $notify = $hyper->connect->query($select_noti);
 		<li class="nav-item">
 			<div style="color: #fff;"> <?= $points; ?> บาท</div>
 		</li>
-		<li class="nav-item">
-			<a class="nav-link" href="logout">
-				<button class="btn btn-sm rd" type="button">
-					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
-						<path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z" />
-						<path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
-					</svg>
-				</button>
+		<li class="nav-item mb-2">
+			<button class="btn" data-toggle="modal" data-target="#notification">
+				<svg xmlns="http://www.w3.org/2000/svg" style="color: #DFC107;" width="20" height="20" fill="currentColor" class="bi bi-bell-fill" viewBox="0 0 16 16">
+					<path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z" />
+				</svg>
+				<span style="color: #fff;"><?= mysqli_num_rows($notify) ?></span>
+			</button>
+		</li>
+		<li class="nav-item mb-1" style="margin-left: -10px;">
+			<a class="nav-link btn btn-sm rd" href="logout">
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+					<path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z" />
+					<path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
+				</svg>
 			</a>
 		</li>
 	</ul>
@@ -66,7 +72,7 @@ $notify = $hyper->connect->query($select_noti);
 				<span style="color: #fff;"><?= mysqli_num_rows($notify) ?></span>
 			</button>
 		</li>
-		<li class="nav-item">
+		<li class="nav-item" style="margin-left: -10px;">
 			<a class="nav-link" href="logout">
 				<button class="btn btn-sm rd" type="button">
 					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
