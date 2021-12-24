737dd5f386a7020fd8e2c6a4c214516791ba3177
DisEzMike <68768636+DisEzMike@users.noreply.github.com>
2021-12-24 23:07:43 +0700

    add datathai class

--- page/admin/history_pay.php
+++ page/admin/history_pay.php
@@ -2,7 +2,7 @@
       <h3 class="text-center mt-4 mb-4" style="color: white;">--- ประวัติรายได้ทั้งหมด ---</h3>
 
       <div class="table-responsive mt-3">
-        <table id="datatable" class="table table-hover text-center w-100">
+        <table id="myTable" class="table table-hover text-center w-100">
           <thead class="hyper-bg-dark">
             <tr>
               <th scope="col" style="width:120px;">เลขที่รายการ</th>
@@ -12,47 +12,8 @@
               <th scope="col" style="width: 170px;">วันที่-เวลา</th>
             </tr>
           </thead>
-          <tbody id="loading" style="display: none;">
-
-            <?php
-            $loaddata = 0;
-            $sql_select_pay = "SELECT * FROM history_pay";
-            $query_pay = $hyper->connect->query($sql_select_pay);
-            $total_pay_row = mysqli_num_rows($query_pay);
-
-            if ($total_pay_row > 0) {
-              $pay = mysqli_fetch_array($query_pay);
-              do {
-            ?>
-                <tr>
-                  <td><?= $pay['pay_id']; ?></th>
-                  <td><?= $pay['username']; ?></th>
-                  <td><?= $pay['link']; ?></th>
-                  <td><?= number_format($pay['amount'], 0); ?></th>
-                  <td><?= $pay['date']; ?></th>
-                </tr>
-              <?php
-                $loaddata++;
-              } while ($pay = mysqli_fetch_array($query_pay));
-              if ($loaddata == $total_pay_row) {
-              ?>
-                <script>
-                  $("#loading").show();
-                </script>
-              <?php
-              } else {
-              ?>
-                <tr>
-                  <td colspan="6">
-                    <div class="spinner-border" role="status">
-                    </div>
-                  </td>
-                </tr>
-            <?php
-              }
-            } ?>
-
-
+          <tbody id="body">
+          </tbody>
           </tbody>
         </table>
       </div>
@@ -79,3 +40,55 @@
           background-color: #ddd;
         }
       </style>
+      <script>
+        $(document).ready(async () => {
+          let isSandbox = window.location.origin == "https://sandbox.dexystore.me";
+          let host = window.location.origin == "http://localhost" ? "http://localhost/dexxystore" : isSandbox ? "https://sandbox.dexystore.me" : "https://dexystore.me";
+          let url = host + '/plugin/getincome.php';
+          let getid = window.location.search.split('id=')[1] || false;
+          $.ajax({
+
+            type: "POST",
+            url: url,
+            dataType: "json",
+            data: {
+              action: 'getincome',
+              type: 1
+            },
+            success: function(json) {
+              if (json.code == 200) {
+                const data = json.data;
+                let body = $('#body').html();
+                let html = '';
+                for (let i = 0; i < data.length; i++) {
+                  body += `
+                  <tr>
+                    <td>${i+1}</th>
+                    <td>${data[i].username}</th>
+                    <td>${data[i].link}</th>
+                    <td>${data[i].amount}</th>
+                    <td>${data[i].date}</th>
+                  </tr>
+                   `;
+                }
+                $('#body').html(body);
+                $('#myTable').DataTable();
+                $('#loading').remove();
+              }
+            },
+            error: function(data) {
+              console.log(data.responseText);
+              $('#myTable').DataTable();
+              html =
+                `
+                <tr>
+                    <td colspan="6">ไม่มีข้อมูลในขณะนี้</td>
+                </tr>
+                `
+              $('#body').html(html);
+              $('#loading').remove();
+            }
+          });
+
+        })
+      </script>
