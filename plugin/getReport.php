2952fcfb818016133f5a46720335ab387fc97808
DisEzMike <68768636+DisEzMike@users.noreply.github.com>
2021-12-24 22:25:32 +0700

    no desc

--- plugin/getReport.php
+++ plugin/getReport.php
@@ -5,7 +5,7 @@ $data = [];
 if ($_POST) {
     if ($_POST['action'] == 'getreport') {
         if ($_POST['type'] == 1) {
-            $select_claim = "SELECT * FROM data_claim WHERE confirm!=9 ORDER BY id DESC LIMIT 20";
+            $select_claim = "SELECT * FROM data_claim WHERE confirm!=9 ORDER BY id DESC";
             $claim_result = $hyper->connect->query($select_claim);
             $i = 0;
             $row = mysqli_num_rows($claim_result);
