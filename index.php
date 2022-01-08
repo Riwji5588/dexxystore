b39a1473be7a0e7023686cf8b8fd58bc04792df7
DisEzMike <mikenatchapon@gmail.com>
2022-01-08 07:37:25 +0700

    x

--- index.php
+++ index.php
@@ -91,7 +91,7 @@ if (isset($_COOKIE['USER_SID'])) {
   <script src="./assets/js/main.js"></script>
 
 
-  
+
   <!-- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script> -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
