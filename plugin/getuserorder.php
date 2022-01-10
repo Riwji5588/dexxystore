a1ce9d6e02a97f70eaea7c885dc1fd5de95324fb
DisEzMike <mikenatchapon@gmail.com>
2022-01-10 15:46:00 +0700

    get user's order

--- plugin/getuserorder.php
+++ plugin/getuserorder.php
@@ -6,7 +6,22 @@ date_default_timezone_set("Asia/Bangkok");
 
 if (isset($_POST)) {
     $ac_id = $_POST['ac_id'];
-    $sellect_order = "SELECT * FROM data_claim WHERE ac_id={$ac_id}";
-    $order = $hyper->connect->query($sellect_order)->fetch_assoc();
-    $now = strtotime(date('Y-m-d H:i:s'));
+    $sellect_order = "SELECT selled_id, exp_date FROM data_selled WHERE ac_id={$ac_id}";
+    $result = $hyper->connect->query($sellect_order);
+    $row = mysqli_num_rows($result);
+    $i = 0;
+    do {
+        $order = $result->fetch_assoc();
+        $now = strtotime(date('Y-m-d H:i:s'));
+        $order_date = strtotime(date($order['exp_date']));
+
+        $expire = $order_date - $now;
+        if ($expire > 0) {
+            array_push($orders, (int)$order['selled_id']);
+        }
+
+        $i++;
+    } while ($row > $i);
+
+    echo json_encode($orders);
 }
