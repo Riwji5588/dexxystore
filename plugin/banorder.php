31593800add20cc65c758a4496d3b9862942fb2d
DisEzMike <mikenatchapon@gmail.com>
2022-01-10 15:49:35 +0700

    ban order

--- plugin/banorder.php
+++ plugin/banorder.php
@@ -3,5 +3,9 @@
 include('./hyper_api.php');
 
 if (isset($_POST)) {
-    $order_id = $_POST['order_id'];
+    $orders_id = explode(',', $_POST['selled_id']);
+    foreach ($orders_id as $order_id) {
+        $ban = "UPDATE data_selled SET ban=1 WHERE selled_id = {$order_id}";
+        $hyper->connect->query($ban);
+    }
 }
