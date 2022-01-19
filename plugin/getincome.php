1198bda8d3106f3f2424acaa086e11df25206740
DisEzMike <mikenatchapon@gmail.com>
2022-01-19 14:07:18 +0700

    update error message

--- plugin/getincome.php
+++ plugin/getincome.php
@@ -24,30 +24,34 @@ if (isset($_POST)) {
     } else if ($_POST['action'] == 'todayincome') {
         if (isset($_POST['type'])) {
             $type = $_POST['type'];
-            if ($type == 0 || $type == 1) {
-                $today = date("Y-m-d", strtotime("-{$type} day"));
-                $sdate = $today . ' 00:00:00';
-                $edate = $today . ' 23:59:59';
-            } else {
-                $range = date("Y-m-d", strtotime("-{$type} day"));
-                $today = date("Y-m-d", strtotime("today"));
-                $sdate = $range . ' 00:00:00';
-                $edate = $today . ' 23:59:59';
-            }
-            $sql_select_pay = "SELECT username, link, amount, date FROM history_pay WHERE date BETWEEN '" . $sdate . "' AND '" . $edate . "'";
-            $query_pay = $hyper->connect->query($sql_select_pay);
-            $total_pay_row = mysqli_num_rows($query_pay);
-            $i = 0;
-            if ($total_pay_row > 0) {
-                do {
-                    $pay = $query_pay->fetch_assoc();
-                    $data[] = $pay;
-                    $i++;
-                } while ($i < $total_pay_row);
-            }
+            if ($type != "") {
+                if ($type == 0 || $type == 1) {
+                    $today = date("Y-m-d", strtotime("-{$type} day"));
+                    $sdate = $today . ' 00:00:00';
+                    $edate = $today . ' 23:59:59';
+                } else {
+                    $range = date("Y-m-d", strtotime("-{$type} day"));
+                    $today = date("Y-m-d", strtotime("today"));
+                    $sdate = $range . ' 00:00:00';
+                    $edate = $today . ' 23:59:59';
+                }
+                $sql_select_pay = "SELECT username, link, amount, date FROM history_pay WHERE date BETWEEN '" . $sdate . "' AND '" . $edate . "'";
+                $query_pay = $hyper->connect->query($sql_select_pay);
+                $total_pay_row = mysqli_num_rows($query_pay);
+                $i = 0;
+                if ($total_pay_row > 0) {
+                    do {
+                        $pay = $query_pay->fetch_assoc();
+                        $data[] = $pay;
+                        $i++;
+                    } while ($i < $total_pay_row);
+                }
 
-            if (!($total_pay_row == count($data))) {
-                $errormsg = "Have error in query";
+                if (!($total_pay_row == count($data))) {
+                    $errormsg = "Have error in query";
+                }
+            } else {
+                $errormsg = "Type is empty";
             }
         } else {
             $errormsg = "Invalid Type Request";
