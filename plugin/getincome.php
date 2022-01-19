ab997bf1118ee69cb0db2501c3a4635ac1991d56
DisEzMike <mikenatchapon@gmail.com>
2022-01-19 13:45:38 +0700

    update getincome

--- plugin/getincome.php
+++ plugin/getincome.php
@@ -1,6 +1,7 @@
 <?php
 include("hyper_api.php");
 $data = [];
+$errormsg = "";
 
 if (isset($_POST)) {
     if ($_POST['action'] == 'getincome') {
@@ -17,43 +18,49 @@ if (isset($_POST)) {
             } while ($i < $total_pay_row);
         }
 
-        if ($total_pay_row == count($data)) {
-            echo json_encode(['code' => 200, 'data' => $data]);
-        } else {
-            echo json_encode(['code' => 500, 'data' => 'Error']);
+        if (!($total_pay_row == count($data))) {
+            $errormsg = "Have error in query";
         }
     } else if ($_POST['action'] == 'todayincome') {
-        $type = $_POST['type'];
-        if ($type == 0 || $type == 1) {
-            $today = date("Y-m-d", strtotime("-{$type} day"));
-            $sdate = $today . ' 00:00:00';
-            $edate = $today . ' 23:59:59';
-        } else {
-            $range = date("Y-m-d", strtotime("-{$type} day"));
-            $today = date("Y-m-d", strtotime("today"));
-            $sdate = $range . ' 00:00:00';
-            $edate = $today . ' 23:59:59';
-        }
-        $sql_select_pay = "SELECT username, link, amount, date FROM history_pay WHERE date BETWEEN '" . $sdate . "' AND '" . $edate . "'";
-        $query_pay = $hyper->connect->query($sql_select_pay);
-        $total_pay_row = mysqli_num_rows($query_pay);
-        $i = 0;
-        if ($total_pay_row > 0) {
-            do {
-                $pay = $query_pay->fetch_assoc();
-                $data[] = $pay;
-                $i++;
-            } while ($i < $total_pay_row);
-        }
+        if (isset($_POST['type'])) {
+            $type = $_POST['type'];
+            if ($type == 0 || $type == 1) {
+                $today = date("Y-m-d", strtotime("-{$type} day"));
+                $sdate = $today . ' 00:00:00';
+                $edate = $today . ' 23:59:59';
+            } else {
+                $range = date("Y-m-d", strtotime("-{$type} day"));
+                $today = date("Y-m-d", strtotime("today"));
+                $sdate = $range . ' 00:00:00';
+                $edate = $today . ' 23:59:59';
+            }
+            $sql_select_pay = "SELECT username, link, amount, date FROM history_pay WHERE date BETWEEN '" . $sdate . "' AND '" . $edate . "'";
+            $query_pay = $hyper->connect->query($sql_select_pay);
+            $total_pay_row = mysqli_num_rows($query_pay);
+            $i = 0;
+            if ($total_pay_row > 0) {
+                do {
+                    $pay = $query_pay->fetch_assoc();
+                    $data[] = $pay;
+                    $i++;
+                } while ($i < $total_pay_row);
+            }
 
-        if ($total_pay_row == count($data)) {
-            echo json_encode(['code' => 200, 'data' => $data]);
+            if (!($total_pay_row == count($data))) {
+                $errormsg = "Have error in query";
+            }
         } else {
-            echo json_encode(['code' => 500, 'data' => 'Error']);
+            $errormsg = "Invalid Type Request";
         }
     } else {
-        echo "Invalid Request";
+        $errormsg = "Invalid Request";
     }
 } else {
-    echo json_encode(['code' => 500, 'data' => 'Error']);
+    $errormsg = "Do not have any data";
+}
+
+if (empty($errormsg)) {
+    echo json_encode(['code' => 200, 'data' => $data]);
+} else {
+    echo json_encode(['code' => 500, 'data' => $errormsg]);
 }
