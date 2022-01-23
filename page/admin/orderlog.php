234df35be8e91eef670c471b4e5defa757c26523
DisEzMike <mikenatchapon@gmail.com>
2022-01-23 21:14:14 +0700

    front end of log phase 1

---
+++ page/admin/orderlog.php
@@ -0,0 +1,482 @@
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content card mx-2">
+            <div class="modal-body card-body">
+                <div class="container">
+                    <form id="form">
+                        <div class="row">
+                            <div class="input-group m-3">
+                                <input id="orderid" type="text" class="form-control" placeholder="เลขออเดอร์" aria-label="เลขออเดอร์" aria-describedby="requestorderid" required>
+                                <div class="input-group-append">
+                                    <button class="btn btn-outline-primary change" type="submit" id="requestorderid">แสดงประวัติ</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="backdroppp"></div>
+
+<h3 class="text-center mt-4 mb-5" style="color: white;">--- ประวัติออเดอร์ย้อนหลัง ---</h3>
+
+<div class="container align-items-center">
+    <div class="row">
+        <div class="col-12">
+            <div id="loader" class="d-flex justify-content-center">
+                <div class="spinner-border text-primary" role="status">
+                    <span class="sr-only">Loading...</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-12">
+            <div class="float-right" role="status">
+                <button class="btn btn-danger mx-auto mb-3" onclick="reorder()"><i class="fas fa-redo"></i> เปลี่ยนออเดอร์</button>
+            </div>
+        </div>
+        <div class="col-12 col-md-3">
+            <div class="card align-items-center mb-3">
+                <div id="mySidebar" class="sidebar card-body">
+                    <a href="#detail" onclick="opentab('detail')"><i class="far fa-address-card"></i> ข้อมูล</a>
+                    <a href="#logs" onclick="opentab('logs')"><i class="fas fa-history"></i> ประวัติย้อนหลัง</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-9">
+            <div id="detail" class="card active">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-12 mb-2">
+                            <center>
+                                <h4>ข้อมูลปัจจุบันของออเดอร์&nbsp;
+                                    <span id="orderidreq">
+                                        <div class="spinner-border text-primary" role="status">
+                                            <span class="sr-only">Loading...</span>
+                                        </div>
+                                    </span>
+                                </h4>
+                            </center>
+                        </div>
+                        <div class="col-12 mb-2" align="center">
+                            <img src="assets/img/item/" id="cardimg" width="99px" height="99px" class="ml-auto mr-auto mb-2" style="border-radius: 50%"></br>
+                            <font class="text-muted" id="carddetail"></font>
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <div class="form-group">
+                                <label for="username">Username</label>
+                                <div class="col-12 p-0">
+                                    <input type="text" class="form-control" id="username" value="Undefine">
+                                    <button class="btn btn-dark btn-sm w-100" type="button" onclick="copy(this,'username')"><i class='far fa-copy'></i> คัดลอก</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <div class="col-12 p-0">
+                                    <input type="text" class="form-control" id="password" value="Undefine">
+                                    <button class="btn btn-dark btn-sm w-100" type="button" onclick="copy(this,'password')"><i class='far fa-copy'></i> คัดลอก</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <div class="form-group">
+                                <label for="display">จอ</label>
+                                <div class="col-12 p-0">
+                                    <input type="text" class="form-control" id="display" value="Undefine">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <div class="form-group">
+                                <label for="banstatus">สถานะการแบน</label>
+                                <div class="col-12 p-0">
+                                    <input type="text" class="form-control" readonly id="banstatus" value="Undefine">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <div class="form-group">
+                                <label for="status">สถานะ</label>
+                                <div class="col-12 p-0">
+                                    <input type="text" class="form-control" readonly id="status" value="Undefine">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <div class="form-group">
+                                <label for="selled_date">วันที่ซื้อ</label>
+                                <div class="col-12 p-0">
+                                    <input type="date" class="form-control" id="selled_date" value="Undefine">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6">
+                            <div class="form-group">
+                                <label for="exp_date">วันหมดประกัน</label>
+                                <div class="col-12 p-0">
+                                    <input type="date" class="form-control" id="exp_date" value="Undefine">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <hr>
+                        </div>
+                        <div class="col-12">
+                            <div class="row justify-content-end">
+                                <button type="button" class="btn btn-danger btn-sm mx-2" onclick="DelData()"><i class="fal fa-trash-alt mr-1"></i> ลบข้อมูล</button>
+                                <button type="button" class="btn btn-primary btn-sm mx-2" onclick="copyfile(this)"><i class="far fa-copy"></i> คัดลอก</button>
+                                <button type="button" class="btn btn-success btn-sm mx-2" onclick="updatedata()"><i class="far fa-plus-square"></i> อัพเดทข้อมูล</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="logs" class="card">
+                <div class="card-body">
+                    Hello, Logs
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- <button class="btn btn-dark btn-sm w-100" type="button" onclick="copyfile(this)"><i class='far fa-copy'></i> คัดลอก</button> -->
+<input type="hidden" id="copyall" value="">
+
+<script>
+    const data = [];
+    $(document).ready(function() {
+        const check_order = window.location.href.split('&order=')[1] ? true : false;
+        if (!check_order) {
+            $('#staticBackdrop').modal('show');
+        } else {
+            $('.backdroppp').remove();
+            const orderid = window.location.href.split('&order=')[1].split('#')[0];
+            console.log(orderid);
+            getlog(orderid);
+            $('#loader').remove();
+        }
+    });
+
+    function getlog(orderid) {
+        $.ajax({
+            url: "plugin/getorderlog.php",
+            type: "POST",
+            dataType: "json",
+            data: {
+                action: "getlog",
+                orderid: orderid
+            },
+            success: function(json) {
+                let code = json.code;
+                if (code == 200) {
+                    console.log(json);
+                    const present = json.data[0].present;
+                    data.claim = json.data[0].claim;
+                    data.claim_first = json.data[0].claim_first;
+                    data.renew = json.data[0].renew;
+
+                    console.log(present.selled_id);
+                    const username = present.username;
+                    const password = atob(present.password);
+                    const display = present.display;
+                    const selled_date = new Date(present.selled_date);
+                    const exp_date = new Date(present.exp_date);
+                    const now = new Date();
+                    const status = (exp_date.getTime() - now.getTime()) > 0 ? 'ยังไม่หมดประกัน' : 'หมดประกัน';
+
+
+
+                    $('#orderidreq').html(present.selled_id)
+
+                    document.getElementById('cardimg').src += present.image_name;
+                    $('#carddetail').html(present.card_title + '-' + present.card_price);
+                    $('#username').val(username);
+                    $('#password').val(password);
+                    $('#display').val(display);
+                    $('#banstatus').val(present.ban == 1 ? 'ถูกแบน' : 'ไม่ได้ถูกแบน');
+                    $('#status').val(status);
+                    $('#selled_date').val(selled_date.toISOString().split('T')[0]);
+                    $('#exp_date').val(exp_date.toISOString().split('T')[0]);
+                } else {
+                    const present = json.present;
+
+                    const username = present.username;
+                    const password = atob(present.password);
+                    const display = present.display;
+                    const selled_date = new Date(present.selled_date);
+                    const exp_date = new Date(present.exp_date);
+                    const now = new Date();
+                    const status = (exp_date.getTime() - now.getTime()) > 0 ? 'ยังไม่หมดประกัน' : 'หมดประกัน';
+
+                    $('#orderidreq').html(present.selled_id)
+                    document.getElementById('cardimg').src += present.image_name;
+                    $('#carddetail').html(present.card_title + '-' + present.card_price);
+                    $('#username').val(username);
+                    $('#password').val(password);
+                    $('#display').val(display);
+                    $('#banstatus').val(present.ban == 1 ? 'ถูกแบน' : 'ไม่ได้ถูกแบน');
+                    $('#status').val(status);
+                    $('#selled_date').val(selled_date.toISOString().split('T')[0]);
+                    $('#exp_date').val(exp_date.toISOString().split('T')[0]);
+                    $('#logs').html(`
+                    <div class="card">
+                        <div class="card-body text-center">
+                            <h4>${json.message}</h4>
+                        </div>
+                    </div>
+                    `);
+                }
+            },
+            error: function(xhr, status, error) {
+                console.log(xhr.responseText);
+            }
+        })
+    }
+
+    $("#form").submit(function(e) {
+        e.preventDefault();
+        orderid = $("#orderid").val();
+        window.location.href = "./orderlog&order=" + orderid;
+
+    });
+
+    function opentab(tab) {
+        $('.card').removeClass('active');
+        $('#' + tab).addClass('active');
+        if (tab == 'logs') {
+            loadlog();
+        }
+    }
+
+    function reorder() {
+        window.location.href = "./orderlog";
+    }
+
+    function loadlog() {
+        console.log(data);
+    }
+
+    function copy(input, id) {
+        var copyText = document.getElementById(id)
+        copyText.select();
+        copyText.setSelectionRange(0, 99999)
+        document.execCommand("copy");
+        input.innerHTML = "<i class='far fa-copy'></i> คัดลอกแล้ว";
+        input.className = "btn btn-success btn-sm w-100";
+        setTimeout(function() {
+            input.innerHTML = "<i class='far fa-copy'></i> คัดลอก";
+            input.className = "btn btn-dark btn-sm w-100";
+        }, 2000);
+    }
+
+    function copyfile(input) {
+        let value = $('#username').val() + ":" + $('#password').val() + " " + $('#display').val()
+        $('#copyall').val(value);
+        let copyText = document.getElementById(`copyall`);
+        copyText.type = "text";
+        copyText.select();
+        copyText.setSelectionRange(0, 99999)
+        document.execCommand("copy");
+        copyText.type = "hidden";
+        input.innerHTML = "<i class='far fa-copy'></i> คัดลอกแล้ว";
+        input.className = "btn btn-success btn-sm mx-2";
+        setTimeout(function() {
+            input.innerHTML = "<i class='far fa-copy'></i> คัดลอก";
+            input.className = "btn btn-dark btn-sm mx-2";
+        }, 1000);
+    }
+
+    /** Delete Data */
+    function DelData() {
+        var id = window.location.href.split('&order=')[1].split('#')[0];
+        swal({
+                title: 'ต้องการลบข้อมูลที่ ' + id,
+                text: "ถ้าลบแล้วจำไม่สามารถกู้กลับมาได้",
+                icon: "warning",
+                buttons: {
+                    confirm: {
+                        text: 'ลบข้อมูล',
+                        className: 'hyper-btn-notoutline-danger'
+                    },
+                    cancel: 'ยกเลิก'
+                },
+                closeOnClickOutside: false,
+            })
+            .then((willDelete) => {
+                if (willDelete) {
+                    $.ajax({
+
+                        type: "POST",
+                        url: "plugin/del_owner_data.php",
+                        dataType: "json",
+                        data: {
+                            id: id
+                        },
+
+                        beforeSend: function() {
+                            swal("กำลังลบข้อมูล กรุณารอสักครู่...", {
+                                button: false,
+                                closeOnClickOutside: false,
+                                timer: 500,
+                            });
+
+                        },
+
+                        success: function(data) {
+                            setTimeout(() => {
+                                if (data.code == "200") {
+                                    swal("ลบข้อมูล สำเร็จ!", "ระบบกำลังพาท่านไป...", "success", {
+                                        button: false,
+                                        closeOnClickOutside: false,
+                                    });
+                                    setTimeout(function() {
+                                        window.location.reload();
+                                    }, 1500);
+                                } else {
+                                    swal(data.msg, "", "error", {
+                                        button: {
+                                            className: 'hyper-btn-notoutline-danger',
+                                        },
+                                        closeOnClickOutside: false,
+                                    });
+                                }
+                            }, 600);
+
+                        }
+
+                    });
+                }
+            });
+    }
+</script>
+
+<style>
+    .active {
+        display: block !important;
+    }
+
+    #detail {
+        display: none;
+    }
+
+    #logs {
+        display: none;
+    }
+
+    /* The sidebar menu */
+    .sidebar {
+        height: 100%;
+        /* 100% Full-height */
+        width: auto;
+        /* 0 width - change this with JavaScript */
+        position: relative;
+        /* Stay in place */
+        z-index: 1;
+        /* Stay on top */
+        top: 0;
+        left: 0;
+        /* Black*/
+        overflow-x: hidden;
+        /* Disable horizontal scroll */
+        padding-top: 15px;
+        padding-bottom: 15px;
+        /* Place content 60px from the top */
+        transition: 0.5s;
+        /* 0.5 second transition effect to slide in the sidebar */
+
+        /* text-align: center; */
+        border-radius: 15px;
+    }
+
+    /* The sidebar links */
+    .sidebar a {
+        padding: 8px 8px 8px 8px;
+        text-decoration: none;
+        font-size: 18px;
+        color: #000;
+        display: block;
+        transition: 0.3s;
+    }
+
+    /* When you mouse over the navigation links, change their color */
+    .sidebar a:hover {
+        color: rgba(0, 0, 0, 0.5);
+    }
+
+    /* Position and style the close button (top right corner) */
+    .sidebar .closebtn {
+        position: absolute;
+        top: 0;
+        right: 25px;
+        font-size: 36px;
+        margin-left: 50px;
+    }
+
+    /* The button used to open the sidebar */
+    .openbtn {
+        font-size: 20px;
+        cursor: pointer;
+        background-color: #111;
+        color: white;
+        padding: 10px 15px;
+        border: none;
+    }
+
+    .openbtn:hover {
+        background-color: #444;
+    }
+
+    /* Style page content - use this if you want to push the page content to the right when you open the side navigation */
+    #main {
+        transition: margin-left .5s;
+        /* If you want a transition effect */
+        padding: 20px;
+    }
+
+    /* On smaller screens, where height is less than 450px, change the style of the sidenav (less padding and a smaller font size) */
+    @media screen and (max-height: 450px) {
+        .sidebar {
+            padding-top: 15px;
+        }
+
+        .sidebar a {
+            font-size: 18px;
+        }
+    }
+
+    @media screen and (max-width: 768px) {
+        .sidebar {
+            display: inline-flex !important;
+        }
+    }
+
+    .change {
+        color: #007bff !important;
+    }
+
+    .change:hover {
+        color: #fff !important;
+    }
+
+    .backdroppp {
+        display: none !important;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(0, 0, 0, 0.5);
+        z-index: 1030;
+    }
+
+    @media screen and (max-width: 576px) {
+        .backdroppp {
+            display: block !important;
+        }
+    }
+</style>
--- plugin/getorderlog.php
+++ plugin/getorderlog.php
@@ -9,8 +9,45 @@ if ($_POST) {
     if (isset($_POST['action']) && $_POST['action'] == 'getlog') {
         if (isset($_POST['orderid']) && !empty($_POST['orderid'])) {
             $orderid = $_POST['orderid'];
+
+            // present owner
+            $sql = "SELECT selled_id, data_id, ac_id, selled_date, exp_date, ban FROM data_selled WHERE selled_id = '$orderid'";
+            $query_present = $hyper->connect->query($sql);
+            $result_present = $query_present->fetch_assoc();
+
+            $data_id = $result_present['data_id'];
+
+            $sql = "SELECT card_id, username, password, display FROM game_data WHERE data_id = '$data_id'";
+            $query_data = $hyper->connect->query($sql);
+            $result_data = $query_data->fetch_assoc();
+
+            $result_present += [
+                'username' => $result_data['username'],
+                'password' => $result_data['password'],
+                'display' => $result_data['display'],
+            ];
+
+            $card_id = $result_data['card_id'];
+
+            $sql = "SELECT card_title, card_price FROM game_card WHERE card_id = '$card_id'";
+            $query_card = $hyper->connect->query($sql);
+            $card = $query_card->fetch_assoc();
+
+            $result_present += [
+                'card_title' => $card['card_title'],
+                'card_price' => $card['card_price']
+            ];
+
+            $select_img = "SELECT image_name FROM card_image WHERE card_id = '$card_id'";
+            $query_img = $hyper->connect->query($select_img);   
+            $img = mysqli_fetch_assoc($query_img)['image_name'];
+
+            $result_present += [
+                'image_name' => $img
+            ];
+
             // get claim log
-            $sql_select_claim = "SELECT data_id, ac_id, claim_date, confirm FROM data_claim WHERE claim_id = '" . $orderid . "' ORDER BY id ASC";
+            $sql_select_claim = "SELECT data_id, claim_date, confirm FROM data_claim WHERE claim_id = '" . $orderid . "' ORDER BY id ASC";
             $query_claim = $hyper->connect->query($sql_select_claim);
             $row = mysqli_num_rows($query_claim);
             $i = 0;
@@ -23,7 +60,7 @@ if ($_POST) {
             }
 
             // get claim_first
-            $sql_select_claim_first = "SELECT data_id, ac_id, claim_date, confirm FROM data_claim_first WHERE claim_id = '" . $orderid . "' ORDER BY id ASC";
+            $sql_select_claim_first = "SELECT data_id, claim_date, confirm FROM data_claim_first WHERE claim_id = '" . $orderid . "' ORDER BY id ASC";
             $query_claim_first = $hyper->connect->query($sql_select_claim_first);
             $row = mysqli_num_rows($query_claim_first);
             $i = 0;
@@ -47,7 +84,6 @@ if ($_POST) {
                     $i++;
                 } while ($i < $row);
             }
-
         } else {
             $errorMSG = "Order ID is empty";
         }
@@ -58,13 +94,21 @@ if ($_POST) {
     header("Location: 403.php");
 }
 
-if (empty($errorMSG)) {
+if (count($claim) == 0) $claim = null;
+if (count($claim_first) == 0) $claim_first = null;
+if (count($renew) == 0) $renew = null;
+if ($claim == null && $claim_first == null && $renew == null) {
+    $errorMSG = "ไม่พบข้อมูลย้อนหลัง";
+} else {
     $data = array([
+        'present' => $result_present,
         'claim' => $claim,
         'claim_first' => $claim_first,
         'renew' => $renew
     ]);
+}
+if (empty($errorMSG)) {
     echo json_encode(['code' => 200, 'data' => $data]);
 } else {
-    echo json_encode(['code' => 500, 'message' => $errorMSG]);
+    echo json_encode(['code' => 500, 'present' => $result_present, 'message' => $errorMSG]);
 }
