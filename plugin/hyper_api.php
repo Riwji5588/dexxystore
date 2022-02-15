15013ec2fe524bfb99d78922f28853d76c224f7e
DisEzMike <mikenatchapon@gmail.com>
2022-02-15 19:06:23 +0700

    change to localhost

--- plugin/hyper_api.php
+++ plugin/hyper_api.php
@@ -5,8 +5,8 @@ include_once dirname(__FILE__) . '/hyper_class.php';
 $hyper = (object) array(
     "user" => new User,
     "connect" => mysqli_connect($db_cofig['db_host'], $db_cofig['db_user'], $db_cofig['db_pass'], $db_cofig['db_name']),
-    "url" => "https://sandbox.dexystore.me",
-    // "url" => "http://localhost/dexystore",
+    // "url" => "https://dexystore.me",
+    "url" => "http://localhost/dexystore",
     "line"=> new LineMsg,
     "notify" => new Notify,
     "datethai" => new DateThai,
